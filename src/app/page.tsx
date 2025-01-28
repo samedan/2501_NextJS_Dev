28084a4611e7683f732db46fe2a2457b3625f365
John Doe <johndoe@example.com>
2025-01-28 12:05:13 +0100

    Started layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "public/home.jpg";
 
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div className="absolute -z-10 -inset-0">
+        <Image
+          fill
+          style={{ objectFit: "cover" }}
+          src={homeImg}
+          alt="car factory"
+        />
+      </div>
+      HomePage
+    </div>
+  );
 }
